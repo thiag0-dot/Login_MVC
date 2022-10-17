11e264b17e7a228d1ec983d2921f0d92ef3e1f5c
thiag0-dot <thiagodeginofilho@gmail.com>
2022-10-17 09:32:57 -0300

    Quarto Commit - Login funcionando

---
+++ App/View/modules/Login/FormLogin.php
@@ -0,0 +1,8 @@
+<form action="/login/auth" method="post">
+
+    <label>E-mail:</label>
+    <input name="email" type="text" />
+    <label>Senha:</label>
+    <input name="senha" type="password" />
+    <button type="submit">Entrar</button>
+</form>
--- App/config.php
+++ App/config.php
@@ -6,3 +6,4 @@ define('VIEWS', BASEDIR . '/APP/View/modules/');
 $_ENV['db']['host'] = 'localhost:3307';
 $_ENV['db']['user'] = 'root';
 $_ENV['db']['pass'] = 'etecjau';
+$_ENV['db']['database'] = 'db_mvc';
